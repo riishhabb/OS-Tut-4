985dbca75a0e3187562559306a46b1a46b3134ec
ero1197 <139821057+ero1197@users.noreply.github.com>
2025-02-12 22:20:39 -0500

    Update questions.h

--- questions.h
+++ questions.h
@@ -1,10 +1,3 @@
-/*
- * Tutorial 4 Jeopardy Project for SOFE 3950U / CSCI 3020U: Operating Systems
- *
- * Copyright (C) 2015, <GROUP MEMBERS>
- * All rights reserved.
- *
- */
 #ifndef QUESTIONS_H_
 #define QUESTIONS_H_
 
@@ -12,18 +5,9 @@
 
 #define MAX_LEN 256
 #define NUM_CATEGORIES 3
-// The number of questions, you can use this in your functions in
-// questions.c, this can be accessed in questions.c
 #define NUM_QUESTIONS 12
 
-// List of 3 categories as array of strings
-static char categories[NUM_CATEGORIES][MAX_LEN] = {
-    "programming", 
-    "algorithms", 
-    "databases"
-};
-
-// Questions struct for each question
+// Question structure
 typedef struct {
     char category[MAX_LEN];
     char question[MAX_LEN];
@@ -32,23 +16,15 @@ typedef struct {
     bool answered;
 } question;
 
-// An array of 12 questions (4 for each category), initialized in initialize_game
-// this may need to be a pointer if you want it set dynamically
-question questions[NUM_QUESTIONS];
+// Declare global questions array
+extern question questions[NUM_QUESTIONS];
 
-// Initializes the array of questions for the game
-extern void initialize_game(void);
-
-// Displays each of the remaining categories and question dollar values that have not been answered
-extern void display_categories(void);
-
-// Displays the question for the category and dollar value
-extern void display_question(char *category, int value);
-
-// Returns true if the answer is correct for the question for that category and dollar value
-extern bool valid_answer(char *category, int value, char *answer);
-
-// Returns true if the question has already been answered
-extern bool already_answered(char *category, int value);
+// Function prototypes
+void initialize_game(void);
+void display_categories(void);
+void display_question(char *category, int value);
+bool valid_answer(char *category, int value, char *answer);
+bool already_answered(char *category, int value);
+bool all_questions_answered(void);
 
 #endif /* QUESTIONS_H_ */
